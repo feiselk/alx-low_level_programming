c506f9c754c9c6e872c5e745023ee234a627ef90
feiselk <feiselkemal15@gmail.com>
2023-02-21 13:15:42 -0800

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,10 +10,11 @@ void print_alphabet(void)
 
 {
 	char c='a'
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 
-}
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
