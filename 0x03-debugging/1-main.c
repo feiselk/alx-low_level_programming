a9ecbcadd5e97ad2a6aced93ff5ac7e62fccbcc8
feiselk <feiselkemal15@gmail.com>
2023-02-23 07:32:13 -0800

    commented

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 0)
-	{
-		putchar(i);/*the variable isn't incremented*/
-	}
+	/*while (i < 0)*/
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
